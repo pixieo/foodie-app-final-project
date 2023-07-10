f857b4c4a9362cf6f9fcdd5328a4b1226cb3ffcb
pixieo <saratayari07@gmail.com>
2023-07-10 15:12:16 +0300

    Add new components: product and waste

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Product from "./Product";
 import Testimonials from "./Testimonials";
-import Footer from "./Footer";
+import Waste from "./Waste";
 import Contact from "./Contact";
+import Footer from "./Footer";
 
 const HomePage = () => {
     return (
         <>
             <Navbar></Navbar>
+            <Product></Product>
+            <Waste></Waste>
             <Testimonials></Testimonials>
             <Contact></Contact>
             <Footer></Footer>
