581b827458f6f693ad18a00b82b4e0e22af841c0
pixieo <saratayari07@gmail.com>
2023-07-10 15:53:13 +0300

    add folder for better organization

---
+++ frontend/src/components/homepage-components/Reviews.jsx
@@ -0,0 +1,146 @@
+import {
+    Box,
+    Flex,
+    Heading,
+    Text,
+    Stack,
+    Container,
+    Avatar,
+    useColorModeValue
+  } from "@chakra-ui/react"
+  
+  const Review = ({ children }) => {
+    return <Box>{children}</Box>
+  }
+  
+  const ReviewContent = ({ children }) => {
+    return (
+      <Stack
+        bg={useColorModeValue("white", "gray.800")}
+        boxShadow={"lg"}
+        p={8}
+        rounded={"xl"}
+        align={"center"}
+        pos={"relative"}
+        _after={{
+          content: `""`,
+          w: 0,
+          h: 0,
+          borderLeft: "solid transparent",
+          borderLeftWidth: 16,
+          borderRight: "solid transparent",
+          borderRightWidth: 16,
+          borderTop: "solid",
+          borderTopWidth: 16,
+          borderTopColor: useColorModeValue("white", "gray.800"),
+          pos: "absolute",
+          bottom: "-16px",
+          left: "50%",
+          transform: "translateX(-50%)"
+        }}
+      >
+        {children}
+      </Stack>
+    )
+  }
+  
+  const ReviewHeading = ({ children }) => {
+    return (
+      <Heading as={"h3"} fontSize={"2xl"}>
+        {children}
+      </Heading>
+    )
+  }
+  
+  const TestimonialText = ({ children }) => {
+    return (
+      <Text
+        textAlign={"center"}
+        color={useColorModeValue("gray.600", "gray.400")}
+        fontSize={"sm"}
+      >
+        {children}
+      </Text>
+    )
+  }
+  
+  const ReviewAvatar = ({ src, name, country }) => {
+    return (
+      <Flex align={"center"} mt={10} direction={"column"}>
+        <Avatar src={src} alt={name} mb={2} />
+        <Stack spacing={-1} align={"center"}>
+          <Text fontWeight={600}>{name}</Text>
+          <Text fontSize={"sm"} color={useColorModeValue("gray.600", "gray.400")}>
+            {country}
+          </Text>
+        </Stack>
+      </Flex>
+    )
+  }
+  
+  export default function Reviews() {
+    return (
+      <Box bg={useColorModeValue("gray.100", "gray.700")} id="reviews">
+        <Container maxW={"7xl"} py={36} as={Stack} spacing={70}>
+          <Stack spacing={3} align={"center"}>
+            <Heading>Our Clients Speak</Heading>
+            <Text>We are helping thousands of people all over the world cook better, save more and waste less.</Text>
+          </Stack>
+          <Stack
+            direction={{ base: "column", md: "row" }}
+            spacing={{ base: 10, md: 4, lg: 20 }}
+          >
+            <Review>
+              <ReviewContent>
+                <ReviewHeading>Efficient App</ReviewHeading>
+                <TestimonialText>
+                  Thank you for this application that helps me avoid wasting food.
+                  This will change my cooking habits.
+                </TestimonialText>
+              </ReviewContent>
+              <ReviewAvatar
+                src={
+                  "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
+                }
+                name={"Nadia"}
+                country={"France"}
+              />
+            </Review>
+            <Review>
+              <ReviewContent>
+                <ReviewHeading>Intuitive Design</ReviewHeading>
+                <TestimonialText>
+                  This is a very straightforward app for making recipies out of ingredients 
+                  you have and it's the best one I have tried so far.
+                </TestimonialText>
+              </ReviewContent>
+              <ReviewAvatar
+                src={
+                  "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
+                }
+                name={"Sara"}
+                country={"Spain"}
+              />
+            </Review>
+            <Review>
+              <ReviewContent>
+                <ReviewHeading>Mindblowing Service</ReviewHeading>
+                <TestimonialText>
+                  First app to ever offer me exactly what I was looking for, recipies for what I have in stock.
+                  
+                </TestimonialText>
+              </ReviewContent>
+              <ReviewAvatar
+                src={
+                  "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
+                }
+                name={"Alessandra"}
+                country={"Italy"}
+              />
+            </Review>
+          </Stack>
+        </Container>
+      </Box>
+    )
+  }
+  
